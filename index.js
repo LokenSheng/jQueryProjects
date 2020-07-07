28b4211fe138862437137d5439ffb70d3138219c
LokenSheng <1252431372@qq.com>
2020-07-07 19:11:09 +0800

    commit

---
+++ index.js
@@ -0,0 +1,41 @@
+(function (data) {
+    var $Wrapper = $('.wrapper');
+    var $ShowSection  = $Wrapper.find('.showSection');
+    var colorsArray = ['#f54545', '#ff8547', '#ffac38'];
+    var curPage = 0;
+    var totalPage = Math.ceil( data.length / 10 );
+    $ShowSection.hide();
+
+    function bindEvent () {
+        $Wrapper.find('.change').on('click', function () {
+            curPage =  ++curPage % totalPage;
+
+            renderPage(data);
+        });
+    }
+
+    function renderPage (data) {
+        $ShowSection.hide().find('.showItem').remove();
+
+        var len = (data.length - curPage * 10) >= 10 ? 10 : data.length - curPage * 10;
+
+        for (var i = 0; i < len; i++) {
+            var $Clone = $Wrapper.find('.tpl').clone().removeClass('tpl').addClass('showItem');
+            var ele = data[i + curPage * 10];
+
+            $Clone.children('span').eq(0)
+                .text(i + curPage * 10 + 1).css('backgroundColor', curPage == 0 && colorsArray[i + curPage])
+                    .next()
+                        .text(ele.title)
+                            .next(ele.search)
+                                .addClass(ele.search > ele.hisSearch ? 'up' : 'down');
+
+            $ShowSection.append($Clone);                   
+        }
+        $ShowSection.fadeIn();
+    }
+
+    bindEvent();
+    renderPage(data);
+
+})(data);
